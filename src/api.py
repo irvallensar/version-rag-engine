513ac0565965490513bd4401a90b985f5b222106
irvallensar <irvallenmikha@gmail.com>
2026-09-12 05:37:49 +0900

    add 'https://version-rag-engine.vercel.app' to ALLOWED_ORIGINS

--- src/api.py
+++ src/api.py
@@ -17,7 +17,7 @@ ALLOWED_ORIGINS = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
